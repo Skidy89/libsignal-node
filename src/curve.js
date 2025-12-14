3edf69b97a89c2c4d3aebc70518b261f76bca876
skidy89 <tutorialesyg310@gmail.com>
2025-12-14 03:12:10 -0600

    changes, c binding to rust yayaya

--- src/curve.js
+++ src/curve.js
@@ -1,8 +1,5 @@
 
 'use strict';
-
-const curve25519 = require('../src/curve25519_wrapper');
-const nodeCrypto = require('crypto');
 const curve25519Rust = require('libsignal-plugins');
 
 
@@ -18,58 +15,19 @@ function validatePrivKey(privKey) {
     }
 }
 
-function scrubPubKeyFormat(pubKey) {
-    if (!(pubKey instanceof Buffer)) {
-        throw new Error(`Invalid public key type: ${pubKey.constructor.name}`);
-    }
-    if (pubKey === undefined || ((pubKey.byteLength != 33 || pubKey[0] != 5) && pubKey.byteLength != 32)) {
-        throw new Error("Invalid public key");
-    }
-    if (pubKey.byteLength == 33) {
-        return pubKey.subarray(1);
-    } else {
-        return pubKey;
-    }
-}
 
-exports.createKeyPairDeprecated = function(privKey) {
-    validatePrivKey(privKey);
-    const keys = curve25519.keyPair(privKey);
-    // prepend version byte
-    var origPub = new Uint8Array(keys.pubKey);
-    var pub = new Uint8Array(33);
-    pub.set(origPub, 1);
-    pub[0] = 5;
-    return {
-        pubKey: Buffer.from(pub),
-        privKey: Buffer.from(keys.privKey)
-    };
-};
+
 exports.createKeyPair = function(privKey) {
+    validatePrivKey(privKey);
     const keys = curve25519Rust.keyPair(privKey);
-    const version = Buffer.alloc(33);
-    version[0] = 5;
-    keys.pubKey.copy(version, 1);
     return {
-        pubKey: version,
+        pubKey: keys.pubKey,
         privKey: keys.privKey
     };
 };
 
-exports.calculateAgreementDeprecated = function(pubKey, privKey) {
-    pubKey = scrubPubKeyFormat(pubKey);
-    validatePrivKey(privKey);
-    if (!pubKey || pubKey.byteLength != 32) {
-        throw new Error("Invalid public key");
-    }
-    return Buffer.from(curve25519.sharedSecret(pubKey, privKey));
-};
+
 exports.calculateAgreement = function(pubKey, privKey) {
-    pubKey = scrubPubKeyFormat(pubKey);
-    validatePrivKey(privKey);
-    if (!pubKey || pubKey.byteLength != 32) {
-        throw new Error("Invalid public key");
-    }
     return curve25519Rust.sharedSecret(pubKey, privKey);
 };
 
@@ -79,28 +37,16 @@ exports.calculateSignature = function(privKey, message) {
     if (!message) {
         throw new Error("Invalid message");
     }
-    return Buffer.from(curve25519.sign(privKey, message));
+    return curve25519Rust.curve25519Sign(privKey, message);
 };
+
 
 exports.verifySignature = function(pubKey, msg, sig, isInit) {
-    pubKey = scrubPubKeyFormat(pubKey);
-    if (!pubKey || pubKey.byteLength != 32) {
-        throw new Error("Invalid public key");
-    }
-    if (!msg) {
-        throw new Error("Invalid message");
-    }
-    if (!sig || sig.byteLength != 64) {
-        throw new Error("Invalid signature");
-    }
-    return isInit ? true : curve25519.verify(pubKey, msg, sig);
+    return curve25519Rust.verifySignature(pubKey, msg, sig, isInit);
 };
 
 
-exports.generateKeyPairDeprecated = function() {
-    const privKey = nodeCrypto.randomBytes(32);
-    return exports.createKeyPair(privKey);
-};
+
 exports.generateKeyPair = function() {
     return curve25519Rust.generateKeyPair();
 };
--- src/keyhelper.js
+++ src/keyhelper.js
@@ -1,7 +1,7 @@
 // vim: ts=4:sw=4:expandtab
 
 const curve = require('./curve');
-const nodeCrypto = require('crypto');
+const plugins = require('libsignal-plugins');
 
 function isNonNegativeInteger(n) {
     return (typeof n === 'number' && (n % 1) === 0  && n >= 0);
@@ -9,27 +9,17 @@ function isNonNegativeInteger(n) {
 
 exports.generateIdentityKeyPair = curve.generateKeyPair;
 
-exports.generateRegistrationId = function() {
-    var registrationId = Uint16Array.from(nodeCrypto.randomBytes(2))[0];
-    return registrationId & 0x3fff; // 0x3fff is the maximum value for a 14-bit integer
-};
+exports.generateRegistrationId = plugins.generateRegistrationId;
 
 exports.generateSignedPreKey = function(identityKeyPair, signedKeyId) {
-    if (!(identityKeyPair.privKey instanceof Buffer) ||
-        identityKeyPair.privKey.byteLength != 32 ||
-        !(identityKeyPair.pubKey instanceof Buffer) ||
-        identityKeyPair.pubKey.byteLength != 33) {
-        throw new TypeError('Invalid argument for identityKeyPair');
-    }
-    if (!isNonNegativeInteger(signedKeyId)) {
-        throw new TypeError('Invalid argument for signedKeyId: ' + signedKeyId);
-    }
-    const keyPair = curve.generateKeyPair();
-    const sig = curve.calculateSignature(identityKeyPair.privKey, keyPair.pubKey);
+    const d = plugins.generateSignedPreKey(identityKeyPair, signedKeyId);
     return {
-        keyId: signedKeyId,
-        keyPair: keyPair,
-        signature: sig
+        keyId: d.keyId,
+        keyPair: {
+            pubKey: d.pubKey,
+            privKey: d.privKey
+        },
+        signature: d.signature
     };
 };
 
